a0e189ec3afab1305759e7cbb4811e1dd1e0eb47
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-19 12:25:14 +0800

    Add key input to move player x direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float runSpeed = 10;
+
     private Vector2 moveInput;
+    private Rigidbody2D myRigidbody;
 
     private void Start()
     {
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
+        Run();
     }
 
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    private void Run()
+    {
+        var velocityY = myRigidbody.velocity.y;
+        var velocityX = moveInput.x * runSpeed;
+
+        var playerVelocity = new Vector2(velocityX, velocityY);
+        myRigidbody.velocity = playerVelocity;
     }
 }
