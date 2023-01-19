11f48495bbda0a77a94dbb18cd48926734fa8278
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-19 12:50:05 +0800

    Transition players sprite to running when moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
     [SerializeField] private float runSpeed = 10;
 
     private Vector2 moveInput;
+    private Animator myAnimator;
     private Rigidbody2D myRigidbody;
 
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -31,12 +34,15 @@ public class PlayerMovement : MonoBehaviour
 
         var playerVelocity = new Vector2(velocityX, velocityY);
         myRigidbody.velocity = playerVelocity;
+
+        var doesPlayerHasHorizontalSpeed = Mathf.Abs(velocityX) > Mathf.Epsilon;
+        if (doesPlayerHasHorizontalSpeed) myAnimator.SetBool(IsRunning, true);
     }
 
     private void FlipSprite()
     {
         var velocityX = myRigidbody.velocity.x;
-        var playerHasHorizontalSpeed = Mathf.Abs(velocityX) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed) transform.localScale = new Vector2(Mathf.Sign(velocityX), 1f);
+        var doesPlayerHasHorizontalSpeed = Mathf.Abs(velocityX) > Mathf.Epsilon;
+        if (doesPlayerHasHorizontalSpeed) transform.localScale = new Vector2(Mathf.Sign(velocityX), 1f);
     }
 }
