5613dcb5ed1543290ad0945178f85c1293b7a2dc
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-21 10:51:44 +0800

    Play audio clip after picking up coin

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] private AudioClip coinPickupSFX;
+    [SerializeField] private float coinVolume = 0.3f;
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("Player")) Destroy(gameObject);
+        if (col.gameObject.CompareTag("Player"))
+        {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position, coinVolume);
+            Destroy(gameObject);
+        }
     }
 }
